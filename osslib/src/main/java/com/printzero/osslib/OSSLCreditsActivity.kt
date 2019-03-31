033f15a3b1a2fd7dd1fc11138f3607261d4fa5be
codekid <ckrackerx15@gmail.com>
2019-03-31 23:10:46 +0530

    activity: new credits screen for ossl library

---
+++ osslib/src/main/java/com/printzero/osslib/OSSLCreditsActivity.kt
@@ -0,0 +1,12 @@
+package com.printzero.osslib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OSSLCreditsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_osslcredits)
+    }
+}
