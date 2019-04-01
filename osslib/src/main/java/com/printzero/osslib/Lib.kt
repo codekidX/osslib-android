39cc9cb367ed907738787697568b9d2c894b3cba
codekid <ckrackerx15@gmail.com>
2019-04-01 23:47:31 +0530

    model: make provision for the upcoming library change

--- osslib/src/main/java/com/printzero/osslib/Lib.kt
+++ osslib/src/main/java/com/printzero/osslib/Lib.kt
@@ -1,5 +1,7 @@
 package com.printzero.osslib
 
+data class Ossl(val projects: MutableList<Lib>, val credits: MutableList<Credit>)
+
 data class Lib(
     val name:String,
     val description: String,
@@ -9,3 +11,5 @@ data class Lib(
     val license_exp: String)
 
 data class License(val key:String, val name: String, val spdx_id: String, val url: String)
+
+data class Credit(val name: String, val url: String, val alias: String, val image: String)
